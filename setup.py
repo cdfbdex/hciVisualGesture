6af40f6dec34a2ee24bcc866d3bee5425e9ac03a
Carlos Ferrin <cdfbdex@gmail.com>
2019-08-24 11:05:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup, find_packages
 
+setup(name='hcivisualgesture',
+      version='0.1.0',
+      description='HCI based on Computer Vision',
+      url='',
+      author='Project: Asistente Virtual (Unicatolica Lumen Gentium)',
+      author_email='cdfbdex@gmail.com',
+      license='BSD (3-clause)',
+      packages=find_packages(),
+      install_requires=['opencv-python', 'scipy', 'scikit-learn',  'joblib', 'pandas'],
+      zip_safe=False)
