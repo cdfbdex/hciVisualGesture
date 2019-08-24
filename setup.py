3f5391bf794d3a8eb45be9ea0884057d94c881d2
Carlos Ferrin <cdfbdex@gmail.com>
2019-08-24 11:32:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
 setup(name='hcivisualgesture',
       version='0.1.0',
       description='HCI based on Computer Vision',
@@ -9,5 +14,5 @@ setup(name='hcivisualgesture',
       author_email='cdfbdex@gmail.com',
       license='BSD (3-clause)',
       packages=find_packages(),
-      install_requires=parse_requirements('requirements.txt', session='hack'),
+      install_requires=parse_requirements('requirements.txt'),
       zip_safe=False)
